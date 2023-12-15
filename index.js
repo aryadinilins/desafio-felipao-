249ea9782b84ae3e874bd13903a0086a2a9a42b8
aryadini <aryadinil@gmail.com>
2023-12-15 15:59:30 -0300

    primeiro teste

---
+++ index.js
@@ -0,0 +1,14 @@
+let nome; Ego 
+let xp; platina
+switch (dia) {
+    case "ouro":
+      console.log("entre 6001 e 7.000");
+      break;
+    case "platina":
+      console.log("entre 7001 e 8.000");
+      break;
+    case "imortal":
+      console.log("entre 9001 e 10.000");
+      break;
+}
+console.log("O Herói de nome " + nome + "está no nivel" + xp )
